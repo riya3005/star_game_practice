bddd4b65f9e7e0d16e0a139e1093e5ca13e67dfd
Priyanka Aggarwal <riya@Priyankas-MacBook-Air.local>
2019-10-01 00:13:15 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
+import Game from "./components/Game.js"
+
+
+// STAR MATCH - V9
+
+
+
+const StarMatch = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StarMatch />
   );
 }
 
 export default App;
+
